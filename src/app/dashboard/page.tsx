a96bf26b706327412e4b871e53118da9e8c3aee0
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2025-04-22 15:20:55 +0200

    testing getUser from database

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,11 @@
+'use client'
 import React from 'react'
+import {useUser} from "@clerk/nextjs";
 
 const DashboardPage = () => {
+    const {user} = useUser()
   return (
-    <div>
-        DashboardPage
-    </div>
+    <div>{user?.firstName}</div>
   )
 }
 

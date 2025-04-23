f86bda120dde26dff7abfacb8fee7655b73e845d
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2025-04-23 15:00:28 +0200

    working on sideBar

--- src/app/(protected)/dashboard/app-sidebar.tsx
+++ src/app/(protected)/dashboard/app-sidebar.tsx
@@ -1,9 +1,73 @@
 'use client'
 
+import {
+    Sidebar,
+    SidebarHeader,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarGroupContent,
+    SidebarMenuItem, SidebarMenuButton
+} from "@/components/ui/sidebar";
+import { LayoutDashboard, Bot, Presentation, CreditCard } from "lucide-react";
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import {cn} from "@/lib/utils";
+
+const items = [
+    {
+        title: "Dashboard",
+        url: "/dashboard",
+        icon: LayoutDashboard,
+    },
+    {
+        title: "Q&A",
+        url: "/qa",
+        icon: Bot,
+    },
+    {
+        title: "Meetings",
+        url: "/meetings",
+        icon: Presentation,
+    },
+    {
+        title: "Billing",
+        url: "/billing",
+        icon: CreditCard,
+    },
+]
+
 export function AppSidebar () {
+    const pathname = usePathname()
+
     return (
-        <div>
-            Side bar here
-        </div>
+        <Sidebar collapsible="icon" variant="floating">
+            <SidebarHeader>
+                Logo
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>
+                        Application
+                    </SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        {items.map(item => {
+                            return (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <Link href={item.url} className={cn({
+                                            '!bg-primary !text-white': pathname === item.url
+                                        })}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            )
+                        })}
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
     )
 }
